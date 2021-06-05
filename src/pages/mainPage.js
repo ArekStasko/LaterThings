203a221b2c8962101e8f9ecdaa6ddcd204ececfb
ArekStasko <arek.stasko@gmail.com>
2021-06-05 22:18:42 +0200

    Add basic structure

---
+++ src/pages/mainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/styles/themes/default/theme.scss'
+
+const MainPage = () => {
+
+    return(
+        <div>
+            <h1>Main Page</h1>
+        </div>
+    )
+}
+
+export default MainPage
